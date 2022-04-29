a9f5050d2517e52682c5746d4803d12df591d38e
Stepan <34548685+stsln@users.noreply.github.com>
2022-04-29 23:00:48 +0300

    Обновление.

--- files_rc.py
+++ files_rc.py
@@ -6731,13 +6731,10 @@ qt_resource_struct = b"\
 \x00\x00\x01\x80p\xd9\x9d\xd5\
 "
 
-
 def qInitResources():
     QtCore.qRegisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
 
-
 def qCleanupResources():
     QtCore.qUnregisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
-
 
 qInitResources()
