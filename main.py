485d419304ae9a1725886510e4eae5b589377caa
stsln <stepan.burimov@yandex.ru>
2022-05-01 22:55:05 +0300

    Переработал кнопки добавления файлов.

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import os
 import sys
 
-from PySide6.QtWidgets import QApplication, QMainWindow, QMessageBox, QFileDialog, QListWidgetItem, \
-                                            QWidget, QVBoxLayout, QHBoxLayout, QToolButton, QPushButton
-from PySide6 import QtCore, QtGui
+from PySide6.QtWidgets import QApplication, QMainWindow, QMessageBox, QListWidgetItem, QPushButton, QToolButton
+from PySide6 import QtCore, QtGui, QtWidgets
 
 import parser_hex_files
 from design import Ui_Main
@@ -12,41 +11,58 @@ hex_files = {}
 data_hex = parser_hex_files.ParserHex()
 
 
-class ToolButtonFile(QWidget):
-    def __init__(self, obj_name, path_icon, parent=None):
+class ToolButtonFile(QtWidgets.QWidget):
+    def __init__(self, obj_name, path_icon, files_lt, parent=None):
         super(ToolButtonFile, self).__init__(parent)
-        self.toolButton = QToolButton()
-        self.toolButton.setObjectName(obj_name)
-        self.toolButton.setIcon(QtGui.QIcon(path_icon))
-        self.toolButton.setIconSize(QtCore.QSize(60, 60))
-        self.toolButton.setMinimumSize(QtCore.QSize(120, 120))
-        self.toolButton.setToolButtonStyle(QtCore.Qt.ToolButtonTextUnderIcon)
-        self.toolButton.setText('Добавить')
 
-        lt = QVBoxLayout(self)
-        lt.addWidget(self.toolButton)
+        size_policy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        size_policy.setHorizontalStretch(0)
+        size_policy.setVerticalStretch(0)
+
+        self.file_btn = QToolButton(files_lt)
+        self.file_btn.setObjectName(obj_name)
+        self.file_btn.setIcon(QtGui.QIcon(path_icon))
+        self.file_btn.setIconSize(QtCore.QSize(60, 60))
+        self.file_btn.setMinimumSize(QtCore.QSize(120, 120))
+        self.file_btn.setToolButtonStyle(QtCore.Qt.ToolButtonTextUnderIcon)
+        self.file_btn.setText('Добавить')
+        self.file_btn.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.file_btn.setChecked(False)
+        self.file_btn.setSizePolicy(size_policy)
+        size_policy.setHeightForWidth(self.file_btn.sizePolicy().hasHeightForWidth())
+
+        lt = QtWidgets.QVBoxLayout(self)
+        lt.setSpacing(0)
+        lt.setContentsMargins(0, 0, 0, 0)
+        lt.addWidget(self.file_btn)
 
 
-class WidgetFile(QWidget):
-    def __init__(self, obj_name, parent=None):
+class WidgetFile(QtWidgets.QWidget):
+    def __init__(self, obj_name, files_lt, parent=None):
         super(WidgetFile, self).__init__(parent)
 
-        self.btn = ToolButtonFile(obj_name, "icons/file_on.png")
-        self.btn.toolButton.clicked.connect(lambda: print(self.btn.toolButton.objectName()))
+        self.btn = ToolButtonFile(obj_name, 'icons/file_off.png', files_lt)
 
-        vll = QVBoxLayout(self)
-        vll.addWidget(self.btn)
-        vll.addStretch()
+        vtv = QtWidgets.QVBoxLayout(self)
+        vtv.addWidget(self.btn)
+        vtv.setSpacing(0)
+        vtv.setContentsMargins(0, 0, 0, 0)
+        vtv.addStretch()
 
-        button = QPushButton('×', self.btn.toolButton)
-        button.setStyleSheet('#buttonClose {font-size: 20px; font-weight: 600; border-radius: 15px; border-color: #fff}'
-                             '#buttonClose:hover {color: white; background: rgba(229, 46, 46, 0.6);}'
-                             '#buttonClose:pressed {color: white; background: rgba(229, 46, 46, 0.8);}')
-        button.setMinimumSize(30, 30)
-        button.setMaximumSize(30, 30)
-        button.setObjectName('buttonClose')
-        button.move(89, 1)
-        button.clicked.connect(lambda: print(button.objectName()))
+        btn_cls = QPushButton('r', self.btn.file_btn)
+        btn_cls.setMinimumSize(30, 30)
+        btn_cls.setMaximumSize(30, 30)
+        btn_cls.setObjectName('btn_cls')
+        btn_cls.move(1, 1)
+        btn_cls.hide()
+        btn_cls.setStyleSheet('#btn_cls {font-family: "Webdings"; font-weight: 400;'
+                              'border-radius: 15px; border-color: #fff}'
+                              '#btn_cls:hover {color: white; background: rgba(229, 46, 46, 0.6);}'
+                              '#btn_cls:pressed {color: white; background: rgba(229, 46, 46, 0.8);}')
+
+        vth = QtWidgets.QHBoxLayout(self)
+        vth.addItem(QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum))
+        vth.addWidget(btn_cls)
 
 
 class Main(QMainWindow):
@@ -55,19 +71,49 @@ class Main(QMainWindow):
         self.ui = Ui_Main()
         self.ui.setupUi(self)
 
+        wgt_fl_1 = WidgetFile('btn_file_1', self.ui.files)
+        btn_fl_1 = wgt_fl_1.btn
+        btn_fl_1_cls = wgt_fl_1.findChildren(QPushButton)[0]
+
+        wgt_fl_2 = WidgetFile('btn_file_2', self.ui.files)
+        btn_fl_2 = wgt_fl_2.btn
+        btn_fl_2_cls = wgt_fl_2.findChildren(QPushButton)[0]
+
+        wgt_fl_3 = WidgetFile('btn_file_3', self.ui.files)
+        btn_fl_3 = wgt_fl_3.btn
+        btn_fl_3_cls = wgt_fl_3.findChildren(QPushButton)[0]
+
+        wgt_fl_4 = WidgetFile('btn_file_4', self.ui.files)
+        btn_fl_4 = wgt_fl_4.btn
+        btn_fl_4_cls = wgt_fl_4.findChildren(QPushButton)[0]
+
+        wgt_fl_5 = WidgetFile('btn_file_5', self.ui.files)
+        btn_fl_5 = wgt_fl_5.btn
+        btn_fl_5_cls = wgt_fl_5.findChildren(QPushButton)[0]
+
+        self.ui.files_lt.addWidget(wgt_fl_1)
+        self.ui.files_lt.addWidget(wgt_fl_2)
+        self.ui.files_lt.addWidget(wgt_fl_3)
+        self.ui.files_lt.addWidget(wgt_fl_4)
+        self.ui.files_lt.addWidget(wgt_fl_5)
+
         QtGui.QFontDatabase.addApplicationFont('fonts/Inter-Regular.ttf')
         QtGui.QFontDatabase.addApplicationFont('fonts/Inter-Bold.ttf')
         QtGui.QFontDatabase.addApplicationFont('fonts/JetBrainsMono-Regular.ttf')
 
-        btn_file_10 = WidgetFile('btn_file_10')
-        self.ui.files_lt.addWidget(btn_file_10)
-
         # buttons file
-        self.ui.btn_file_1.clicked.connect(lambda: self.choose_file(self.ui.btn_file_1))
-        self.ui.btn_file_2.clicked.connect(lambda: self.choose_file(self.ui.btn_file_2))
-        self.ui.btn_file_3.clicked.connect(lambda: self.choose_file(self.ui.btn_file_3))
-        self.ui.btn_file_4.clicked.connect(lambda: self.choose_file(self.ui.btn_file_4))
-        self.ui.btn_file_5.clicked.connect(lambda: self.choose_file(self.ui.btn_file_5))
+        btn_fl_1.findChildren(QToolButton)[0].clicked.connect(lambda: self.choose_file(btn_fl_1, btn_fl_1_cls))
+        btn_fl_2.findChildren(QToolButton)[0].clicked.connect(lambda: self.choose_file(btn_fl_2, btn_fl_2_cls))
+        btn_fl_3.findChildren(QToolButton)[0].clicked.connect(lambda: self.choose_file(btn_fl_3, btn_fl_3_cls))
+        btn_fl_4.findChildren(QToolButton)[0].clicked.connect(lambda: self.choose_file(btn_fl_4, btn_fl_4_cls))
+        btn_fl_5.findChildren(QToolButton)[0].clicked.connect(lambda: self.choose_file(btn_fl_5, btn_fl_5_cls))
+
+        # buttons close file
+        btn_fl_1_cls.clicked.connect(lambda: self.close_file(btn_fl_1, btn_fl_1_cls))
+        btn_fl_2_cls.clicked.connect(lambda: self.close_file(btn_fl_2, btn_fl_2_cls))
+        btn_fl_3_cls.clicked.connect(lambda: self.close_file(btn_fl_3, btn_fl_3_cls))
+        btn_fl_4_cls.clicked.connect(lambda: self.close_file(btn_fl_4, btn_fl_4_cls))
+        btn_fl_5_cls.clicked.connect(lambda: self.close_file(btn_fl_5, btn_fl_5_cls))
 
         # button merge
         self.ui.btn_merge.clicked.connect(lambda: self.merge())
@@ -93,9 +139,9 @@ class Main(QMainWindow):
         vs_adr.valueChanged.connect(lambda: self.change_scroll(vs_adr, vs_data))
         vs_data.valueChanged.connect(lambda: self.change_scroll(vs_data, vs_adr))
 
-    def choose_file(self, btn_file):
-        file_path = QFileDialog.getOpenFileName(self, 'Выберите файл для добавления в список', '',
-                                                'Hex Files (*.hex)')[0]
+    def choose_file(self, btn_fl, btn_fl_cls):
+        file_path = QtWidgets.QFileDialog.getOpenFileName(self, 'Выберите файл для добавления в список', '',
+                                                          'Hex Files (*.hex)')[0]
         file_name = os.path.basename(file_path)[0:-4]
         if file_name in hex_files.keys():
             msg_box = QMessageBox()
@@ -114,11 +160,12 @@ class Main(QMainWindow):
                 if flag_err:
                     del hex_files[file_name]
                 else:
-                    if btn_file.text() != 'Добавить':
-                        del hex_files[btn_file.text()]
-                        del data_hex.data_hex_list[btn_file.text()]
-                    btn_file.setText(file_name)
-                    # изменить иконку кнопки
+                    if btn_fl.file_btn.text() != 'Добавить':
+                        del hex_files[btn_fl.file_btn.text()]
+                        del data_hex.data_hex_list[btn_fl.file_btn.text()]
+                    btn_fl.file_btn.setText(file_name)
+                    btn_fl.file_btn.setIcon(QtGui.QIcon('icons/file_on.png'))
+                    btn_fl_cls.show()
                     self.update_list_widget()
 
     def get_data_list_widget(self, check: bool = False):
@@ -252,6 +299,15 @@ class Main(QMainWindow):
     def text_changed(self):
         if len(self.ui.text_ofs_reg.toPlainText()) > 4:
             self.ui.text_ofs_reg.setPlainText(self.ui.text_ofs_reg.toPlainText()[0:4])
+
+    def close_file(self, btn_file, btn_close):
+        text = btn_file.file_btn.text()
+        del hex_files[text]
+        self.update_data()
+        self.update_list_widget()
+        btn_file.file_btn.setText('Добавить')
+        btn_file.file_btn.setIcon(QtGui.QIcon('icons/file_off.png'))
+        btn_close.hide()
 
     @staticmethod
     def split_item_list_widget(item):
